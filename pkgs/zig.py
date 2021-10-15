f6d6991a91bf298d5e0b87aa226a56594c169676
Jonathan Marler <johnnymarler@gmail.com>
2021-10-15 12:06:51 -0600

    update zig package

--- pkgs/zig.py
+++ pkgs/zig.py
@@ -1,76 +1,94 @@
 def zig_template(*, name, version, platform, prebuilt):
 
+    dummy = {
+        'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    }
+
     hashTable = {
         'freebsd-x86_64':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-aarch64':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-armv6kz':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-armv7a':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-i386':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-riscv64':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'linux-x86_64':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'ax6czp2ojvipneukgwpm7iil2p4nbfok',
-            'outHash':'oajtrtqor6fetasn7uc52jlon6yzoy55',
+            '0.6.0':{
+                'inHash':'ax6czp2ojvipneukgwpm7iil2p4nbfok',
+                'outHash':'oajtrtqor6fetasn7uc52jlon6yzoy55',
+            },
+            '0.8.1':{
+                'inHash':'iqe7xn3gz2l64xbubvuzckg7phhhavfc',
+                'outHash':'c4xh6al7ir5kebj6tjl4vq34uipftrs4',
+            },
         },
         'macos-x86_64':{
             'archiveExt':'.tar.xz',
             'exeExt':'',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'windows-i386':{
             'archiveExt':'.zip',
             'exeExt':'.exe',
-            'inHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            'outHash':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '0.6.0': dummy,
+            '0.8.1': dummy,
         },
         'windows-x86_64':{
             'archiveExt':'.zip',
             'exeExt':'.exe',
-            'inHash':'jbaakrh3g5kc6hi7et46jwhnraupy46f',
-            'outHash':'fqtteizbgdgrx6tm3u32l6y6nwwgvrl3',
+            '0.6.0':{
+                'inHash':'jbaakrh3g5kc6hi7et46jwhnraupy46f',
+                'outHash':'fqtteizbgdgrx6tm3u32l6y6nwwgvrl3',
+            },
+            '0.8.1':{
+                'inHash':'gzdwxtsekxlvy4g6m65h3psfezjgiaom',
+                'outHash':'bbuvbedin5737oyzliannguvgl3xo5xg',
+            },
         },
     }
     platformData = hashTable[platform]
     exeExt = platformData['exeExt']
+    platformVersionData = platformData[version]
 
     name = 'zig-' + platform + '-' + version
     return {'config':{'ifaces':['c-compiler','zig-compiler']},'in':[],'ops':[
         {'op':'fetchArchive',
          'url':'https://ziglang.org/download/' + version + '/' + name + platformData['archiveExt'],
-         'inHash':platformData['inHash'],
-         'outHash':platformData['outHash'],
+         'inHash':platformVersionData['inHash'],
+         'outHash':platformVersionData['outHash'],
          'to':'@stage'},
         {'op':'mkdir','path':'@tmpout'},
         {'op':'mkdirs','path':'@tmpout/genesis/i'},
@@ -83,8 +101,7 @@ def zig_template(*, name, version, platform, prebuilt):
     ]}
 
 obj_set.add_or_get_variation_set('zig', zig_template).add({
-    #'version':['0.6.0','0.5.0'],
-    'version':['0.6.0'],
+    'version':['0.6.0', '0.8.1'],
     'platform':[
         'freebsd-x86_64',
         'linux-aarch64',
